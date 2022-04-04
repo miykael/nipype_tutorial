abe50bcb2c34b7ddccb86312fa5bbd6641f7a277
Christopher J. Markiewicz <markiewicz@stanford.edu>
2022-04-04 16:32:33 -0400

    TEST: nipype rel/1.7.1

--- generate.sh
+++ generate.sh
@@ -17,7 +17,7 @@ generate_docker() {
            --miniconda \
              conda_install="python=3.8 pytest jupyter jupyterlab jupyter_contrib_nbextensions
                             traits pandas matplotlib scikit-learn scikit-image seaborn nbformat nb_conda" \
-             pip_install="https://github.com/nipy/nipype/tarball/master
+             pip_install="git+https://github.com/nipy/nipype.git@rel/1.7.1
                           pybids==0.13.1
                           nilearn datalad[full] nipy duecredit nbval niflow-nipype1-workflows" \
              create_env="neuro" \
@@ -56,7 +56,7 @@ generate_singularity() {
            --miniconda \
              conda_install="python=3.8 pytest jupyter jupyterlab jupyter_contrib_nbextensions
                             traits pandas matplotlib scikit-learn scikit-image seaborn nbformat nb_conda" \
-             pip_install="https://github.com/nipy/nipype/tarball/master
+             pip_install="git+https://github.com/nipy/nipype.git@rel/1.7.1
                           pybids==0.13.1
                           nilearn datalad[full] nipy duecredit nbval niflow-nipype1-workflows" \
              create_env="neuro" \
