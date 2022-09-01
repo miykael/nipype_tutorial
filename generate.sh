1836933d07a1cdb129ab8bcc4a7890114a3c5511
mathiasg <mathiasg@stanford.edu>
2022-09-01 15:37:02 -0400

    MAINT: Bump singularity python version

--- generate.sh
+++ generate.sh
@@ -64,7 +64,7 @@ generate_singularity() {
            --user=neuro \
            --workdir /home/neuro \
            --miniconda \
-             conda_install="python=3.7 pytest jupyter jupyterlab jupyter_contrib_nbextensions
+             conda_install="python=3.8 pytest jupyter jupyterlab jupyter_contrib_nbextensions
                             traits pandas matplotlib scikit-learn scikit-image seaborn nbformat nb_conda" \
              pip_install="$NIPYPE_URL
                           pybids==0.13.1
